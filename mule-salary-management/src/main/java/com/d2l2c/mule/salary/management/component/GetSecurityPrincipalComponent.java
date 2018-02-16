54e958b72dfff95ef591a4bdfc8423cd27890522
Dayan Lazare <dayan.lazare@gmail.com>
2018-02-16 18:00:34 -0500

    Create component to retrieve user name.

---
+++ mule-salary-management/src/main/java/com/d2l2c/mule/salary/management/component/GetSecurityPrincipalComponent.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.d2l2c.mule.salary.management.component;
+
+import org.mule.api.MuleEventContext;
+import org.mule.api.lifecycle.Callable;
+import org.mule.api.security.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ * @author dayanlazare
+ *
+ */
+public class GetSecurityPrincipalComponent implements Callable {
+
+	@Override
+	public Object onCall(MuleEventContext eventContext) throws Exception {
+		Authentication auth = eventContext.getSession().getSecurityContext().getAuthentication();
+		UserDetails principal = (UserDetails) auth.getPrincipal();
+		eventContext.getMessage().setInvocationProperty("userName", principal.getUsername());
+		return eventContext.getMessage().getPayload();
+	}
+
+}
